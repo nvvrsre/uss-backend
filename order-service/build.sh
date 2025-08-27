a98a07bb70112a6072cb15f8d841b826f9485712
nvvr-devops <nvvrdevops24@gmail.com>
2025-08-27 16:57:23 +0530

    updated code

--- order-service/build.sh
+++ order-service/build.sh
@@ -1,3 +1,3 @@
-docker build -t nvvrsre/usindus-order-service .
-docker push nvvrsre/usindus-order-service
+docker build -t nvvrsre/order-service .
+docker push nvvrsre/order-service
 kubectl rollout restart deployment/order-service
